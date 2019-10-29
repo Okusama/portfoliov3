324c5ee0ec2ced2170ebb92a17d78422060f045d
Okusama <david.nuad75@live.fr>
2019-10-29 22:53:08 +0100

    Add Font / Home Page Svg

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,28 +10,55 @@ export default class HomePage extends React.Component<HomeState> {
         super(props);
         this.state = {
             presentation_bloc: {
-                path: HomeData.presentation_bloc["1920"].path,
-                color: HomeData.presentation_bloc["1920"].color
+                path: HomeData.presentation_bloc.path,
+                color: HomeData.presentation_bloc.color
             },
             realisation_bloc: {
-                path: "",
-                color: ""
+                path: HomeData.realisation_bloc.path,
+                color: HomeData.realisation_bloc.color
             },
             project_bloc: {
-                path: "",
-                color: ""
+                path: HomeData.project_bloc.path,
+                color: HomeData.project_bloc.color
             },
             contact_bloc: {
-                path: "",
-                color: ""
+                path: HomeData.contact_bloc.path,
+                color: HomeData.contact_bloc.color
             }
         };
     }
 
     render(){
         return(
-            <div>
-                <h1>Home page</h1>
+            <div className="homeMain">
+                <section>
+                    <svg viewBox="0 0 840 420" preserveAspectRatio="xMinyMin meet">
+                        <desc>Présentation de mon profil</desc>
+                        <path d={this.state.presentation_bloc.path} stroke={this.state.presentation_bloc.color} fill="none" fillOpacity="0"/>
+                        <text x="440" y ="210" textAnchor="middle" stroke={this.state.presentation_bloc.color}>Presentation</text>
+                    </svg>
+                </section>
+                <section>
+                    <svg viewBox="0 0 840 420" preserveAspectRatio="xMinyMin meet">
+                        <desc>Voir mes réalisations</desc>
+                        <path d={this.state.realisation_bloc.path} stroke={this.state.realisation_bloc.color} fill="none" fillOpacity="0"/>
+                        <text x="440" y ="210" textAnchor="middle" stroke={this.state.realisation_bloc.color}>Realisation</text>
+                    </svg>
+                </section>
+                <section>
+                    <svg viewBox="0 0 840 420" preserveAspectRatio="xMinyMin meet">
+                        <desc>Voir mes projets</desc>
+                        <path d={this.state.project_bloc.path} stroke={this.state.project_bloc.color} fill="none" fillOpacity="0"/>
+                        <text x="440" y ="210" textAnchor="middle" stroke={this.state.project_bloc.color}>Projets</text>
+                    </svg>
+                </section>
+                <section>
+                    <svg viewBox="0 0 840 420" preserveAspectRatio="xMinyMin meet">
+                        <desc>Me suivre sur Twitter/GitHub/Linkdin ou me laisser un message</desc>
+                        <path d={this.state.contact_bloc.path} stroke={this.state.contact_bloc.color} fill="none" fillOpacity="0"/>
+                        <text x="440" y ="210" textAnchor="middle" stroke={this.state.contact_bloc.color}>Contact</text>
+                    </svg>
+                </section>
             </div>
         );
     }
