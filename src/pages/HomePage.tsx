161bdd65bd588f69304771522d5f0dff6d6c2864
Okusama <david.naud75@live.fr>
2019-10-29 12:42:07 +0100

    Test state interface

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
-export default class HomePage extends React.Component {
+interface HomeState {
+    path: number;
+}
+
+export default class HomePage extends React.Component<HomeState> {
+
+    state: HomeState;
+
+    constructor(props: any){
+        super(props);
+        this.state = {
+            path: 0
+        };
+    }
+
     render(){
         return(
             <div>
                 <h1>Home page</h1>
+                {this.state.path}
             </div>
         );
     }
